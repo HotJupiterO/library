79eaff0908e6b7d1d6bd4562692f4667bd15a863
Yaroslav Repetskyy <jaroslawrepecki@gmail.com>
2023-01-31 14:36:57 +0100

    initial project commit

---
+++ src/main/java/library/util/PersonValidator.java
@@ -0,0 +1,34 @@
+package library.util;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+import library.dao.PersonDAO;
+import library.models.Person;
+
+@Component
+public class PersonValidator implements Validator {
+
+    private final PersonDAO personDAO;
+
+    @Autowired
+    public PersonValidator(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return Person.class.equals(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        Person person = (Person) target;
+        if (personDAO.show(person.getEmail()).isPresent()) {
+            errors.rejectValue("email", "", "This email already exist in the database.");
+        }
+
+    }
+}
